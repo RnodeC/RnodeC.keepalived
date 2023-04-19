c92c4fc612263866eb5c04f44b1b0524ce625a48
rnodec <rnodec@pm.me>
2023-04-19 13:09:25 -0400

    LOG_FILE var

--- files/check_rke2.sh
+++ files/check_rke2.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-rm -f /tmp/check_rke2.log
-touch /tmp/check_rke2.log
+LOG_FILE=/tmp/check_rke2.sh
+
+rm -f $LOG_FILE
+touch $LOG_FILE
 
 NODE_NAME=$(hostname)
 
-echo; echo "[INFO] Checking for node: $NODE_NAME" | tee -a /tmp/checkrke2.log
+echo; echo "[INFO] Checking for node: $NODE_NAME" | tee -a $LOG_FILE
 
 
 # Get the node status from kubectl
 echo; echo "[INFO] These are the nodes that kubectl sees"
-/var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml get nodes | tee -a /tmp/checkrke2.log 
+/var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml get nodes | tee -a $LOG_FILE 
 
 NODE_STATUS=$(/var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml get nodes "$NODE_NAME" --no-headers -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
-echo; echo "[INFO] Node Ready?  $NODE_STATUS | tee -a /tmp/checkrke2.log"
+echo; echo "[INFO] Node Ready?  $NODE_STATUS | tee -a $LOG_FILE"
 
 # Check if the node status is Ready
 if [ "$NODE_STATUS" == "True" ]; then
-    echo; echo "[INFO] success | tee -a /tmp/checkrke2.log"
+    echo; echo "[INFO] success | tee -a $LOG_FILE"
     exit 0
 else
-    echo; echo "[ERROR] Node not ready | tee -a /tmp/checkrke2.log"
+    echo; echo "[ERROR] Node not ready | tee -a $LOG_FILE"
     exit 1
 fi

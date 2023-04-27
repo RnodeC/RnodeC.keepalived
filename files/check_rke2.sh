db18839ba958454c50129e44dfe3f45a5f176d78
rnodec <rnodec@pm.me>
2023-04-27 16:54:16 -0400

    selinux

--- files/check_rke2.sh
+++ files/check_rke2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_FILE=/tmp/keepalived_check_rke2.sh
+LOG_FILE=/tmp/keepalived_check_rke2.log
 
 rm -f $LOG_FILE
 touch $LOG_FILE
@@ -15,13 +15,13 @@ echo; echo "[INFO] These are the nodes that kubectl sees"
 /var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml get nodes | tee -a $LOG_FILE 
 
 NODE_STATUS=$(/var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml get nodes "$NODE_NAME" --no-headers -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
-echo; echo "[INFO] Node Ready?  $NODE_STATUS | tee -a $LOG_FILE"
+echo; echo "[INFO] Node Ready?  $NODE_STATUS" | tee -a $LOG_FILE
 
 # Check if the node status is Ready
 if [ "$NODE_STATUS" == "True" ]; then
-    echo; echo "[INFO] success | tee -a $LOG_FILE"
+    echo; echo "[INFO] success" | tee -a $LOG_FILE
     exit 0
 else
-    echo; echo "[ERROR] Node not ready | tee -a $LOG_FILE"
+    echo; echo "[ERROR] Node not ready" | tee -a $LOG_FILE
     exit 1
 fi
